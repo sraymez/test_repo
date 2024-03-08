a6112340bfec44b7d1558d330caf918ae97c0c09
sraymez <santirg97@gmail.com>
2024-03-08 15:22:00 +0000

    checking whether commit reflected in forked repo

--- leaflet.R
+++ leaflet.R
@@ -1,15 +1,14 @@
-# Making interactive maps with leaflet
+## Making interactive maps with leaflet
 
-# Directory
-setwd("C:/Users/SantiagoRaymentGomez/OneDrive - London School of Hygiene and Tropical Medicine/OneZoo PhD/Data analysis")
+# Environment set up ----
 
 {library(pacman)
 p_load(leaflet, leaflet.extras, sf, tidyverse)}
 
 # Senegal and Gambia shapefiles
-sen_adm0 <- st_read("Shapefiles/gadm41_SEN_0.shp")
-sen_adm1 <- st_read("Shapefiles/gadm41_SEN_1.shp")
-sen_adm2 <- st_read("Shapefiles/gadm41_SEN_2.shp")
+sen_adm0 <- st_read(here("Shapefiles", "gadm41_SEN_0.shp"))
+sen_adm1 <- st_read(here("Shapefiles", "gadm41_SEN_1.shp"))
+sen_adm2 <- st_read(here("Shapefiles", "gadm41_SEN_2.shp"))
 
 # Html code for labeling interactive plot
 labels <- sprintf(
@@ -47,7 +46,7 @@ leaflet(sen_adm1) %>%
   addDrawToolbar() %>% 
   addPolygons(weight = .5, fillColor = "grey")
 
-# species digitiser georeferencing tool ---------------------------------
+# species digitiser georeferencing tool ----
 library(devtools)
 library(remotes)
 
@@ -56,3 +55,5 @@ package_url = "https://cran.r-project.org/src/contrib/Archive/rgdal/rgdal_1.6-7.
 remotes::install_version("rgdal", version = "1.5-18")
 
 install_github("ptarroso/speciesdigitizer", subdir="source")
+
+# End ----
